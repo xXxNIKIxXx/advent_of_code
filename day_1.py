3b8676da7b968d501e8b37219e91e131ad8a35a3
Niklas Fuchs <niklas@fuchs-kiel.de>
2024-12-05 12:07:40 +0100

    Day 1.2 done

--- day_1.py
+++ day_1.py
@@ -7,7 +7,20 @@ def find_floor(instructions):
             floor -= 1
     return floor
 
+def first_enter_basement(instructions):
+    floor = 0
+    for position, char in enumerate(instructions, start=1):
+        if char == '(':
+            floor += 1
+        elif char == ')':
+            floor -= 1
+        if floor == -1:
+            return position
+    return None
+
 with open('day_1.txt') as f:
     instructions = f.read()
 
 print(find_floor(instructions))
+
+print(first_enter_basement(instructions))
