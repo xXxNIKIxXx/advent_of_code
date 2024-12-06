0c31773afe6070ab84235ebddbb40c70a07a67b4
Niklas Fuchs <niklas@fuchs-kiel.de>
2024-12-06 16:18:41 +0100

    Day 2.2 done

--- day_2.py
+++ day_2.py
@@ -13,4 +13,17 @@ for line in lines:
     surface_area = 2 * side1 + 2 * side2 + 2 * side3
     total_paper += surface_area + smallest_side
 
+total_ribbon = 0
+
+for line in lines:
+    line = line.replace("\n", "")
+    l, w, h = map(int, line.split('x'))
+    perimeters = [2 * (l + w), 2 * (w + h), 2 * (h + l)]
+    smallest_perimeter = min(perimeters)
+    volume = l * w * h
+    total_ribbon += smallest_perimeter + volume
+
+
 print(f"Total wrapping paper needed: {total_paper} square feet")
+
+print(f"Total ribbon needed: {total_ribbon} feet")
