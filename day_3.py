c16ffb7e566a82afd585db04e4738889b9058712
Niklas Fuchs <niklas@fuchs-kiel.de>
2024-12-06 16:22:33 +0100

    Day 3.2 done

--- day_3.py
+++ day_3.py
@@ -22,8 +22,45 @@ def count_houses_with_presents(directions):
     # Return the number of unique houses visited
     return len(visited_houses)
 
+
+def count_houses_with_presents_with_robo_santa(directions):
+    # Starting positions for Santa and Robo-Santa
+    santa_x, santa_y = 0, 0
+    robo_x, robo_y = 0, 0
+    # Set to keep track of visited houses
+    visited_houses = set()
+    # Add the starting house
+    visited_houses.add((santa_x, santa_y))
+
+    # Process each direction
+    for i, direction in enumerate(directions):
+        if i % 2 == 0:  # Santa's turn
+            if direction == '^':
+                santa_y += 1
+            elif direction == 'v':
+                santa_y -= 1
+            elif direction == '>':
+                santa_x += 1
+            elif direction == '<':
+                santa_x -= 1
+            visited_houses.add((santa_x, santa_y))
+        else:  # Robo-Santa's turn
+            if direction == '^':
+                robo_y += 1
+            elif direction == 'v':
+                robo_y -= 1
+            elif direction == '>':
+                robo_x += 1
+            elif direction == '<':
+                robo_x -= 1
+            visited_houses.add((robo_x, robo_y))
+
+    # Return the number of unique houses visited
+    return len(visited_houses)
+
 with open('2015/day_3.txt', 'r') as file:
     directions = file.read().strip()
 
 print(count_houses_with_presents(directions))
 
+print(count_houses_with_presents_with_robo_santa(directions))
