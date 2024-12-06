25c73dd97c73e4583ca6e0729f91e979a1300c9e
Niklas Fuchs <niklas@fuchs-kiel.de>
2024-12-07 00:48:32 +0100

    Day 1.1 done

---
+++ day_1.py
@@ -0,0 +1,26 @@
+from collections import Counter
+
+def calculate_total_distance(left_list, right_list):
+    # Sort both lists
+    left_list.sort()
+    right_list.sort()
+    
+    # Calculate the total distance
+    total_distance = sum(abs(l - r) for l, r in zip(left_list, right_list))
+    
+    return total_distance
+
+left_list = []
+right_list = []
+
+with open('2024/day_1.txt') as file:
+    lines = file.readlines()
+
+for line in lines:
+    line = line.replace('\n', '')
+    left_list_num, right_list_num = [int(num) for num in line.split()]
+    left_list.append(left_list_num)
+    right_list.append(right_list_num)
+    
+    
+print(calculate_total_distance(left_list, right_list))
