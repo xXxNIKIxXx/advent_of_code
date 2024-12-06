74f7bb0817a1a0d3771d56012f03b1e7367f8423
Niklas Fuchs <niklas@fuchs-kiel.de>
2024-12-07 00:43:43 +0100

    Day 17.2 done

--- day_17.py
+++ day_17.py
@@ -15,3 +15,19 @@ with open('day_17.txt') as f:
 target = 150
 result = count_combinations(containers, target)
 print(f'Total combinations: {result}')
+
+def min_container_combinations(containers, target):
+    min_count = float('inf')
+    min_combinations = 0
+    for i in range(1, len(containers) + 1):
+        for combo in combinations(containers, i):
+            if sum(combo) == target:
+                if i < min_count:
+                    min_count = i
+                    min_combinations = 1
+                elif i == min_count:
+                    min_combinations += 1
+    return min_combinations
+
+min_combinations = min_container_combinations(containers, target)
+print(f'Minimum container combinations: {min_combinations}')
