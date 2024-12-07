560a0a5ee520e6bb3a8a1012229f86b92bf3c6e7
Niklas Fuchs <niklas@fuchs-kiel.de>
2024-12-07 01:07:36 +0100

    Day 1.2 done

--- day_1.py
+++ day_1.py
@@ -12,3 +12,20 @@ def total_fuel_requirement(filename):
 filename = 'day_1.txt'
 total_fuel = total_fuel_requirement(filename)
 print(f"Total fuel requirement: {total_fuel}")
+
+def calculate_total_fuel(mass):
+    total_fuel = 0
+    while mass > 0:
+        fuel = calculate_fuel(mass)
+        if fuel > 0:
+            total_fuel += fuel
+        mass = fuel
+    return total_fuel
+
+total_fuel = 0
+with open(filename, 'r') as file:
+    for line in file:
+        mass = int(line.strip())
+        total_fuel += calculate_total_fuel(mass)
+
+print(f"Total fuel requirement including fuel mass: {total_fuel}")
