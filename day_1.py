e101e2c1a8d36a348b0f172a3fea323cd2de617a
Niklas Fuchs <niklas@fuchs-kiel.de>
2024-12-07 01:54:42 +0100

    Day 1.2 done

--- day_1.py
+++ day_1.py
@@ -12,3 +12,23 @@ with open('day_1.txt', 'r') as file:
         if line:
             total_sum += get_calibration_value(line)
 print(f"The sum of all calibration values is: {total_sum}")
+
+sum_part2 = 0
+
+integer_names = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+with open('day_1.txt', 'r') as file:
+    for line in file:
+        line = line.strip()
+        if line:
+            nums = []
+            for i, letter in enumerate(line):
+                for val, name in enumerate(integer_names):
+                    if name in line[i:i+len(name)]:
+                        nums.append(str(val))
+                if letter.isdigit():
+                    nums.append(letter)
+            if nums:
+                sum_part2 += int(nums[0] + nums[-1])
+
+print(f"The sum of all part 2 values is: {sum_part2}")
