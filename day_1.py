ca3ec83b905350612167f306eaa05bdd8f138098
Niklas Fuchs <niklas@fuchs-kiel.de>
2024-12-07 01:04:50 +0100

    Day 1.2 done

--- day_1.py
+++ day_1.py
@@ -11,3 +11,16 @@ def calculate_resulting_frequency(changes):
 changes = read_frequency_changes('day_1.txt')
 resulting_frequency = calculate_resulting_frequency(changes)
 print(f"The resulting frequency is {resulting_frequency}")
+
+def find_first_repeated_frequency(changes):
+    frequency = 0
+    seen_frequencies = set([frequency])
+    while True:
+        for change in changes:
+            frequency += change
+            if frequency in seen_frequencies:
+                return frequency
+            seen_frequencies.add(frequency)
+
+first_repeated_frequency = find_first_repeated_frequency(changes)
+print(f"The first frequency reached twice is {first_repeated_frequency}")
