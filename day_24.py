c040dee3f4e3945960983f1bbbfd11f549932cd7
Niklas Fuchs <niklas@fuchs-kiel.de>
2024-12-15 22:06:53 +0100

    Day 24.2 done

--- day_24.py
+++ day_24.py
@@ -26,3 +26,12 @@ if best_group:
     print(f"The quantum entanglement is: {quantum_entanglement}")
 else:
     print("No valid group found")
+    
+group_count = 4
+best_group, quantum_entanglement = find_best_group(weights, group_count)
+
+if best_group:
+    print(f"The best group is: {best_group}")
+    print(f"The quantum entanglement is: {quantum_entanglement}")
+else:
+    print("No valid group found")
